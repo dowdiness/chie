63c7ec7a9f46def049746de37aa96efe8d10bc91
koji Ishimoto <koji.programs@gmail.com>
2019-08-21 00:25:58 +0900

    Add some plugins to make seo friendly site

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,16 +5,44 @@
  */
 
 module.exports = {
+  siteMetadata: {
+    siteUrl: `https://quirky-kepler-11b926.netlify.com`,
+  },
   plugins: [
+    `gatsby-plugin-sitemap`,
+    `gatsby-plugin-robots-txt`,
     `gatsby-plugin-netlify-cms`,
     'gatsby-plugin-postcss', 
     'gatsby-plugin-emotion',
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-netlify`,
     {
       resolve: 'gatsby-plugin-purgecss',
       options: {
         tailwind: true,
       }
     },
+    {
+      resolve: `gatsby-plugin-canonical-urls`,
+      options: {
+        siteUrl: `https://quirky-kepler-11b926.netlify.com`,
+        stripQueryString: true,
+      },
+    },
+    {
+      resolve: "gatsby-plugin-google-tagmanager",
+      options: {
+        id: "GTM-K2R2CL7",
+  
+        // Include GTM in development.
+        // Defaults to false meaning GTM will only be loaded in production.
+        includeInDevelopment: false,
+  
+        // datalayer to be set before GTM is loaded
+        // should be an object or a function that is executed in the browser
+        // Defaults to null
+        defaultDataLayer: { platform: "gatsby" },
+        },  
     {
       resolve: `gatsby-source-filesystem`,
       options: {
